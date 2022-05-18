613458a66034563d9fd4aa0d1e8653083d0d7a24
Sanyyel <102636553+Sanyyel@users.noreply.github.com>
2022-05-18 14:45:35 -0700

    Add files via upload

---
+++ Teclado.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Teclado
+    {
+        private static BufferedReader teclado =
+                new BufferedReader (
+                        new InputStreamReader(
+                                System.in));
+
+        public static String getUmString ()
+        {
+            String ret=null;
+
+            try
+            {
+                ret = teclado.readLine ();
+            }
+            catch (IOException erro)
+            {} // sei que nao vai dar erro
+
+            return ret;
+        }
+
+        public static int getUmInt () throws Exception
+        {
+            int ret=0;
+
+            try
+            {
+                ret = Integer.parseInt (teclado.readLine ());
+            }
+            catch (IOException erro)
+            {} // sei que nao vai dar erro
+            catch (NumberFormatException erro)
+            {
+                throw new Exception ("Int invalido!");
+            }
+
+            return ret;
+        }
+    }
