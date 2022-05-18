d8cafe962a13a9497b0231b7c0fc7b98a9d5d94f
Sanyyel <102636553+Sanyyel@users.noreply.github.com>
2022-05-18 18:45:01 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,17 @@
 import java.io.*;
+import java.util.Objects;
 
-public class Main extends Coordenada{
-    public static void main (String[] args) {
-        BufferedReader in = null;
+public class Main
+{
+    public static void main(String[] args)
+    {
         try {
-            in = new BufferedReader(new FileReader("C:\projetos\untitled\ProjetoLabirinto\src\SemEntrada.txt"));
-            // se for testar acima na sua máquina, ponha o caminho do arquivo nesse FileReader();
-            
-            String str;
-            StringBuilder sb = new StringBuilder(str); // peguei isso pra dar .deteleCharAt() e tentar apagar os números do labirinto
-            while ((str = in.readLine()) != null) {
-                if(Objects.equals(str.charAt(0), "123456789") && Objects.equals(str.charAt(1), "123456789")){
-
-                    sb.deleteCharAt(0); // aqui tento apagar o número do labirinto
-
-
-                    // aqui embaixo eu deveria inserir o mapa numa matriz de caracteres à partir das linhas e colunas
-                    // da classe Coordenada e fazer as restrições: sem entrada, sem saída, E ou S fora da margem e etc,
-                    // o que ainda não cheguei a fazer
-                }
-                /*char[] mapa = str.toCharArray();
-                int l;
-                for(l = 0; l <= mapa.length; l++){
-                    for(c = 0; c <= mapa.length; c++){
-                        System.out.println(mapa [l][c]);
-                    }
-                }
-
-                 */
-            }
-            in.close();
-        } catch (IOException e) {
+            System.out.println("Insira aqui o nome do arquivo do labirinto: ");
+            String arquivo = Teclado.getUmString();
+            Labirinto labirinto = new Labirinto(arquivo);
+            labirinto.resolverLab();
+            System.out.println(labirinto);
         }
-    }
-}
+        catch(Exception erro){
+            System.err.println("Erro: " + erro.getMessage());
+        }
