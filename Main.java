d7084a218e19168ea72acb6c8a946b925974cd9b
Sanyyel <102636553+Sanyyel@users.noreply.github.com>
2022-05-21 17:08:43 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,54 +1,33 @@
+import java.util.Collections;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        Pilha<Coordenada> inverso;
+        Pilha<Coordenada> caminho = new Pilha<>();
         try {
             System.out.println("Insira aqui o nome do arquivo do labirinto: ");
             String arquivo = Teclado.getUmString();
             Labirinto labirinto = new Labirinto(arquivo);
-            labirinto.resolverLab();
+            inverso = labirinto.resolverLab();
             System.out.println(labirinto);
+            try {
+                for (int i = 0; i <= inverso.devolveTamanho(); i++){
+                    System.out.println(inverso.recupereUmItem());
+                    inverso.removaUmItem();
+                }
+            }
+            catch(Exception erro){
+
+            }
         }
         catch(Exception erro){
             System.err.println("Erro: " + erro.getMessage());
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new FileReader("C:\\projetos\\untitled\\ProjetoLabirinto\\src\\SemEntrada.txt"));
-            String str = null;
-            while ((str = in.readLine()) != null) {
-                System.out.println(str);
-            }
-            in.close();
-        } catch (IOException e) {
-        }
-
-         */
-
     }
 
 }
-
